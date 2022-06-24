14f88143dc4dc85e473000227eff5cd5f3de4408
Karol Gutierrez <karol.gutierrez@syscom.mx>
2022-06-24 17:59:30 -0600

    update

--- scripts/script.js
+++ scripts/script.js
@@ -3,41 +3,50 @@ function convertir()
     if($("#privada").val() != "")
     {
         var urlPrivada = $("#privada").val()
-        var a = urlPrivada.indexOf("ftp://")
-        var b = urlPrivada.indexOf("@172.31.13.99/")
-        var usuario = urlPrivada.substring(6, b)
-        var ruta = urlPrivada.substring(b)
-        var rutaCompleta =  ruta.substring(14)
+        var a = urlPrivada.indexOf("@172.31.13.99/")
+        var usuario = urlPrivada.substring(6, a)
+        var rutaCompleta = urlPrivada.substring(a)
+        var ruta =  rutaCompleta.substring(14)
         console.log("Usuario: " + usuario)
-        console.log("Ruta: " + rutaCompleta)
-        var urlPublica = urlPrivada.replace(urlPrivada, "https://ftp3.syscom.mx/usuarios/" + usuario + "/" + rutaCompleta)
+        console.log("Ruta: " + ruta)
+        var urlPublica = urlPrivada.replace(urlPrivada, "https://ftp3.syscom.mx/usuarios/" + usuario + "/" + ruta)
         $("#publica").val(urlPublica)
     }
 }
 
 function copiar()
 {
-    var urlPublica = document.getElementById("publica").value
-    navigator.clipboard.writeText(urlPublica)
-    console.log(urlPublica)
-}
-
-/*function copiar()
-{
-
-    console.log('Funcion de copiar')
     if($("#privada").val() != "")
     {
-        $("#publica").prop({"disabled":false})
-        var urlPublica = document.getElementById("publica")
-        urlPublica.ariaSelected()
-        document.execCommand("copy")
-        alert("Copied!")
+        var urlPublica = document.getElementById("publica").value
+        navigator.clipboard.writeText(urlPublica)
+        console.log("URL Pública: " + urlPublica)
     }
-    else
-    {
-        $("#publica").prop({"disabled":true})
-    }
-}*/
+}
 
-    
+function mensaje()
+{
+    $("#mensaje1").css({"display":"block"})
+}
+
+/*function esperar(espera)
+{
+    string = "pausa_alerta("+ espera +")"
+    setTimeout(string, espera)
+    //mensaje = setTimeout(mensaje, 2000)
+}
+
+function pausa_alerta(espera)
+{
+    alert("ok " + espera/1000 + " segundos")
+    //window.setTimeout(mensaje(), 2000)
+    /*alert("Copiado")
+    clearTimeout(mensaje)*/
+    //$("#mensaje1").prop({"disabled":false})
+/*}*/
+
+function vaciar()
+{
+    document.getElementById("privada").value = ""
+    document.getElementById("publica").value = ""
+}
